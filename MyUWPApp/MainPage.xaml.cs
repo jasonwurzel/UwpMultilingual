95d7de70e6d8ebc7aebdb315d50c654a1e89ee1c
Michael Kreich <michaelkreich@hotmail.com>
2019-09-05 14:44:08 +0200

    Add project files.

---
+++ MyUWPApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
+
+namespace MyUWPApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
